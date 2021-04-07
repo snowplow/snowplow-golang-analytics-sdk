4c3629601c80662cf01c58d023b821075df77476
colmsnowplow <colm@snowplowanalytics.com>
2021-04-07 10:43:57 +0100

    Add analytics sdk 0.1.0-beta.1

--- README.md
+++ README.md
@@ -1,3 +1,125 @@
 # Snowplow Golang Analytics SDK
 
-Coming soon
+[![Build Status][gh-actions-image]][gh-actions] [![Release][release-image]][releases] [![License][license-image]][license]
+
+![snowplow-logo](media/snowplow_logo.png)
+
+Snowplow is a scalable open-source platform for rich, high quality, low-latency data collection. It is designed to collect high quality, complete behavioural data for enterprise business.
+
+## Snowplow Pipeline Overview
+
+![snowplow-pipeline](media/snowplow_architecture.png)
+
+The [Snowplow trackers][tracker-docs] enable highly customisable collection of raw, unopinionated event data. The pipeline validates these events against a JSONSchema - to guarantee a high quality dataset - and adds information via both standard and custom enrichments.
+
+This data is then made available in-stream for real-time processing, and can also be loaded to blob storage and data warehouse for analysis.
+
+The Snowplow atomic data acts as an immutable log of all the actions that occurred across your digital products. The [analytics SDKs][sdk-docs] are libraries in a range languages which facilitate working with Snowplow Enriched data, by transforming it from its original TSV format to a more amenable format for programmatic interaction - for example JSON.
+
+## Try Snowplow
+
+This repo contains an analytics SDK which is relevant to users who already have a full Snowplow pipeline running (which can be done Open Source or via our [Snowplow Insights](https://snowplowanalytics.com/snowplow-insights/) service).
+
+If you don't have a pipeline yet, you might be interested in finding out what Snowplow can do by setting up [Try Snowplow](https://try.snowplowanalytics.com/?utm_source=github&utm_medium=post&utm_campaign=try-snowplow).
+
+## Quickstart
+
+```bash
+go get github.com/snowplow/snowplow-golang-analytics-sdk
+```
+
+```go
+import "github.com/snowplow/snowplow-golang-analytics-sdk/analytics"
+
+parsed, err := ParseEvent(event) // Where event is a valid TSV string Snowplow event.
+if err != nil {
+  fmt.Println(err)
+}
+
+parsed.ToJson() // whole event to JSON
+parsed.ToMap() // whole event to map
+parsed.GetValue("page_url") // get a value for a single canonical field
+parsed.GetSubsetMap("page_url", "domain_userid", "contexts", "derived_contexts") // Get a map of values for a set of canonical fields
+parsed.GetSubsetJson("page_url", "unstruct_event") // Get a JSON of values for a set of canonical fields
+```
+
+## API
+
+```go
+func ParseEvent(event string) (ParsedEvent, error)
+```
+
+ParseEvent takes a Snowplow Enriched event tsv string as input, and returns a 'ParsedEvent' typed slice of strings.
+Methods may then be called on the resulting ParsedEvent type to transform the event, or a subset of the event to Map or Json.
+
+```go
+func (event ParsedEvent) ToJson() ([]byte, error)
+```
+
+ToJson transforms a valid Snowplow ParsedEvent to a JSON object.
+
+```go
+func (event ParsedEvent) ToMap() (map[string]interface{}, error)
+```
+
+ToMap transforms a valid Snowplow ParsedEvent to a Go map.
+
+```go
+func (event ParsedEvent) GetSubsetJson(fields ...string) ([]byte, error)
+```
+
+GetSubsetJson returns a JSON object containing a subset of the event, containing only the atomic fields provided, without processing the rest of the event.
+For custom events and contexts, only "unstruct_event", "contexts", or "derived_contexts" may be provided, which will produce the entire data object for that field.
+For contexts, the resultant map will contain all occurrences of all contexts within the provided field.
+
+```go
+func (event ParsedEvent) GetSubsetMap(fields ...string) (map[string]interface{}, error)
+```
+
+GetSubsetMap returns a map of a subset of the event, containing only the atomic fields provided, without processing the rest of the event.
+For custom events and contexts, only "unstruct_event", "contexts", or "derived_contexts" may be provided, which will produce the entire data object for that field.
+For contexts, the resultant map will contain all occurrences of all contexts within the provided field.
+
+```go
+func (event ParsedEvent) GetValue(field string) (interface{}, error)
+```
+
+GetValue returns the value for a provided atomic field, without processing the rest of the event.
+For unstruct_event, it returns a map of only the data for the unstruct event.
+
+```go
+func (event ParsedEvent) ToJsonWithGeo() ([]byte, error)
+```
+
+ToJsonWithGeo adds the geo_location field, and transforms a valid Snowplow ParsedEvent to a JSON object.
+
+```go
+func (event ParsedEvent) ToMapWithGeo() (map[string]interface{}, error)
+```
+
+ToMapWithGeo adds the geo_location field, and transforms a valid Snowplow ParsedEvent to a Go map.
+
+## Copyright and license
+
+SQL Runner is copyright 2021 Snowplow Analytics Ltd.
+
+Licensed under the **[Apache License, Version 2.0][license]** (the "License");
+you may not use this software except in compliance with the License.
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+[gh-actions]: https://github.com/snowplow/snowplow-golang-analytics-sdk/actions
+[gh-actions-image]: https://github.com/snowplow/snowplow-golang-analytics-sdk/workflows/Test/badge.svg?branch=master
+
+[release-image]: https://img.shields.io/github/v/release/snowplow/snowplow-golang-analytics-sdk?include_prereleases
+[releases]: https://img.shields.io/github/v/release/snowplow/snowplow-golang-tracker
+
+[license-image]: http://img.shields.io/badge/license-Apache--2-blue.svg?style=flat
+[license]: http://www.apache.org/licenses/LICENSE-2.0
+
+[tracker-docs]: https://docs.snowplowanalytics.com/docs/collecting-data/collecting-from-own-applications/
+[sdk-docs]: https://docs.snowplowanalytics.com/docs/modeling-your-data/analytics-sdk/
---
+++ analytics/transform.go
@@ -0,0 +1,256 @@
+//
+// Copyright (c) 2021 Snowplow Analytics Ltd. All rights reserved.
+//
+// This program is licensed to you under the Apache License Version 2.0,
+// and you may not use this file except in compliance with the Apache License Version 2.0.
+// You may obtain a copy of the Apache License Version 2.0 at http://www.apache.org/licenses/LICENSE-2.0.
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the Apache License Version 2.0 is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
+//
+
+package analytics
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/pkg/errors"
+)
+
+const eventLength int = 131
+
+type KeyVal struct {
+	Key   string
+	Value interface{}
+}
+
+type ValueParser func(string, string) ([]KeyVal, error)
+
+type KeyFunctionPair struct {
+	Key           string
+	ParseFunction ValueParser
+}
+
+type ParsedEvent []string
+
+func parseTime(key string, value string) ([]KeyVal, error) {
+	if value == "" {
+		return nil, errors.Wrap(errors.New("Null string found"), fmt.Sprintf("Error parsing key %s", key))
+	}
+	timeLayout := "2006-01-02 15:04:05.999"
+	out, err := time.Parse(timeLayout, value)
+	if err != nil {
+		return nil, errors.Wrap(err, fmt.Sprintf("Error parsing field '%s', with value '%s' to timestamp", key, value))
+	}
+	return []KeyVal{{key, out}}, err
+}
+
+func parseString(key string, value string) ([]KeyVal, error) {
+	if value == "" {
+		return nil, errors.Wrap(errors.New("Null string found"), fmt.Sprintf("Error parsing key %s", key))
+	}
+	return []KeyVal{{key, value}}, nil
+}
+
+func parseInt(key string, value string) ([]KeyVal, error) {
+	if value == "" {
+		return nil, errors.Wrap(errors.New("Null string found"), fmt.Sprintf("Error parsing key %s", key))
+	}
+	intValue, err := strconv.Atoi(value)
+	if err != nil {
+		return nil, errors.Wrap(err, fmt.Sprintf("Error parsing key '%s' to integer", key))
+	}
+	return []KeyVal{{key, intValue}}, err
+}
+
+func parseBool(key string, value string) ([]KeyVal, error) {
+	if value == "" {
+		return nil, errors.Wrap(errors.New("Null string found"), fmt.Sprintf("Error parsing key %s", key))
+	}
+	boolValue, err := strconv.ParseBool(value)
+	if err != nil {
+		return nil, errors.Wrap(err, fmt.Sprintf("Error parsing key '%s' to boolean", key))
+	}
+	return []KeyVal{{key, boolValue}}, err
+}
+
+func parseDouble(key string, value string) ([]KeyVal, error) {
+	if value == "" {
+		return nil, errors.Wrap(errors.New("Null string found"), fmt.Sprintf("Error parsing key %s", key))
+	}
+	doubleValue, err := strconv.ParseFloat(value, 64)
+	if err != nil {
+		return nil, errors.Wrap(err, fmt.Sprintf("Error parsing key '%s' to double", key))
+	}
+	return []KeyVal{{key, doubleValue}}, err
+}
+
+func parseContexts(key string, value string) ([]KeyVal, error) {
+	if value == "" {
+		return nil, errors.Wrap(errors.New("Null string found"), fmt.Sprintf("Error parsing key %s", key))
+	}
+	return shredContexts(value)
+}
+
+func parseUnstruct(key string, value string) ([]KeyVal, error) {
+	if value == "" {
+		return nil, errors.Wrap(errors.New("Null string found"), fmt.Sprintf("Error parsing key %s", key))
+	}
+	return shredUnstruct(value)
+}
+
+// ParseEvent takes a Snowplow Enriched event tsv string as input, and returns a 'ParsedEvent' typed slice of strings. 
+// Methods may then be called on the resulting ParsedEvent type to transform the event, or a subset of the event to Map or Json.
+func ParseEvent(event string) (ParsedEvent, error) {
+	record := strings.Split(event, "\t")
+	if len(record) != eventLength {
+		return nil, errors.New(fmt.Sprintf("Cannot parse tsv event - wrong number of fields provided: %v", len(event)))
+	}
+	return record, nil
+}
+
+func (event ParsedEvent) mapifyGoodEvent(knownFields [131]KeyFunctionPair, addGeolocationData bool) (map[string]interface{}, error) {
+	if len(event) != eventLength {
+		return nil, errors.New(fmt.Sprintf("Cannot transform event - wrong number of fields provided: %v", len(event)))
+	} else {
+		output := make(map[string]interface{})
+		if addGeolocationData && event[latitudeIndex] != "" && event[longitudeIndex] != "" {
+			output["geo_location"] = event[latitudeIndex] + "," + event[longitudeIndex]
+		}
+		for index, value := range event {
+			// skip if empty
+			if event[index] != "" {
+				// apply function if not empty
+				kvPairs, err := knownFields[index].ParseFunction(knownFields[index].Key, value)
+				if err != nil {
+					return nil, err
+				}
+				// append all results
+				for _, pair := range kvPairs {
+					output[pair.Key] = pair.Value
+				}
+			}
+		}
+		return output, nil
+	}
+}
+
+// ToMap transforms a valid Snowplow ParsedEvent to a Go map.
+func (event ParsedEvent) ToMap() (map[string]interface{}, error) {
+	return event.mapifyGoodEvent(enrichedEventFieldTypes, false)
+}
+
+// ToMapWithGeo adds the geo_location field, and transforms a valid Snowplow ParsedEvent to a Go map.
+func (event ParsedEvent) ToMapWithGeo() (map[string]interface{}, error) {
+	return event.mapifyGoodEvent(enrichedEventFieldTypes, true)
+}
+
+// ToJson transforms a valid Snowplow ParsedEvent to a JSON object.
+func (event ParsedEvent) ToJson() ([]byte, error) {
+
+	mapified, err := event.ToMap()
+	if err != nil {
+		return nil, err
+	}
+
+	jsonified, err := json.Marshal(mapified)
+	if err != nil {
+		return nil, errors.Wrap(err, "Error marshaling to JSON")
+	}
+	return jsonified, nil
+}
+
+// ToJsonWithGeo adds the geo_location field, and transforms a valid Snowplow ParsedEvent to a JSON object.
+func (event ParsedEvent) ToJsonWithGeo() ([]byte, error) {
+	mapified, err := event.ToMapWithGeo()
+	if err != nil {
+		return nil, err
+	}
+
+	jsonified, err := json.Marshal(mapified)
+	if err != nil {
+		return nil, errors.Wrap(err, "Error marshaling to JSON")
+	}
+	return jsonified, nil
+}
+
+// GetValue returns the value for a provided atomic field, without processing the rest of the event.
+// For unstruct_event, it returns a map of only the data for the unstruct event.
+func (event ParsedEvent) GetValue(field string) (interface{}, error) {
+
+	if len(event) != eventLength {
+		return nil, errors.New(fmt.Sprintf("Cannot get value - wrong number of fields provided: %v", len(event)))
+	}
+	index, ok := indexMap[field]
+	if !ok {
+		return nil, errors.New(fmt.Sprintf("Key %s not a valid atomic field", field))
+	}
+	if event[index] == "" {
+		return nil, errors.New(fmt.Sprintf("Field %s is empty", field))
+	}
+	kvPairs, err := enrichedEventFieldTypes[index].ParseFunction(enrichedEventFieldTypes[index].Key, event[index])
+	if err != nil {
+		return nil, err
+	}
+	if field == "contexts" || field == "derived_contexts" || field == "unstruct_event" {
+		// TODO: DRY HERE TOO?
+		output := make(map[string]interface{})
+		for _, pair := range kvPairs {
+			output[pair.Key] = pair.Value
+		}
+		return output, nil
+	}
+	return kvPairs[0].Value, nil
+
+}
+
+// GetSubsetMap returns a map of a subset of the event, containing only the atomic fields provided, without processing the rest of the event.
+// For custom events and contexts, only "unstruct_event", "contexts", or "derived_contexts" may be provided, which will produce the entire data object for that field.
+// For contexts, the resultant map will contain all occurrences of all contexts within the provided field.
+func (event ParsedEvent) GetSubsetMap(fields ...string) (map[string]interface{}, error) {
+
+	if len(event) != eventLength {
+		return nil, errors.New(fmt.Sprintf("Cannot get values - wrong number of fields provided: %v", len(event)))
+	}
+	output := make(map[string]interface{})
+	for _, field := range fields {
+		index, ok := indexMap[field]
+		if !ok {
+			return nil, errors.New(fmt.Sprintf("Key %s not a valid atomic field", field))
+		}
+		if event[index] != "" {
+			kvPairs, err := enrichedEventFieldTypes[index].ParseFunction(enrichedEventFieldTypes[index].Key, event[index])
+			if err != nil {
+				return nil, err
+			}
+			for _, pair := range kvPairs {
+				output[pair.Key] = pair.Value
+			}
+		}
+	}
+	return output, nil
+}
+
+// GetSubsetJson returns a JSON object containing a subset of the event, containing only the atomic fields provided, without processing the rest of the event.
+// For custom events and contexts, only "unstruct_event", "contexts", or "derived_contexts" may be provided, which will produce the entire data object for that field.
+// For contexts, the resultant map will contain all occurrences of all contexts within the provided field.
+func (event ParsedEvent) GetSubsetJson(fields ...string) ([]byte, error) {
+
+	if len(event) != eventLength {
+		return nil, errors.New(fmt.Sprintf("Cannot get values - wrong number of fields provided: %v", len(event)))
+	}
+	subsetMap, err := event.GetSubsetMap(fields...)
+	if err != nil {
+		return nil, err
+	}
+	subsetJson, err := json.Marshal(subsetMap)
+	if err != nil {
+		return nil, err
+	}
+	return subsetJson, nil
+}
