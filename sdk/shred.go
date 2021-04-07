9d93cbb680ada87718f1009b1b6805c6c4d2b237
colmsnowplow <colm@snowplowanalytics.com>
2021-04-07 10:43:57 +0100

    Add outline of code with notes

---
+++ sdk/shred.go
@@ -0,0 +1,150 @@
+package sdk // Terrible name... TODO: Come up with a better name
+
+import (
+	//	"encoding/base64"
+	//	"encoding/csv"
+	"encoding/json"
+	// "errors" // TODO: Decide whether to use this or handle errors another way
+	"fmt"
+	"regexp"
+	"strings"
+	//	"time"
+	"unicode" // For camel to snake case - consider alternative?
+	// 	"github.com/hashicorp/go-multierror" // check out these packages for error handling
+	"github.com/pkg/errors"
+)
+
+// This should be SelfDescribingData...
+type SelfDescribingData struct {
+	Schema string                 `json:"schema"` // Probably don't need the json tags
+	Data   map[string]interface{} `json:"data"`
+
+	// If we keep this as a string instead of a map, will it unmarshal into a string?
+	// If so, I'm guessing it's preferable to do so and then marshal it to json as we assign to keys...
+	// Data string `json:"data"`
+	// OK so - error: json: cannot unmarshal object into Go struct field SelfDescribingData.data.data of type string
+	// using map[string]string does work but still results in a map...
+	// maybe leave it as []byte???
+	// Data []byte - doesn't work.
+
+}
+
+type Contexts struct {
+	Schema string               `json:"schema"`
+	Data   []SelfDescribingData `json:"data"`
+}
+
+type UnstructEvent struct {
+	Schema string
+	Data   SelfDescribingData
+}
+
+type SchemaParts struct {
+	Protocol string
+	Vendor   string
+	Name     string
+	Format   string
+	Model    string
+	Revision string
+}
+
+// Should be const??
+var SCHEMA_URI_REGEX = `(?P<protocol>^iglu:)(?P<vendor>[a-zA-Z0-9-_.]+)/(?P<name>[a-zA-Z0-9-_]+)/(?P<format>[a-zA-Z0-9-_]+)/(?P<model>[1-9][0-9]*)(?P<revision>(?:-(?:0|[1-9][0-9]*)){2}$)`
+// Take regex capture group names out, as not used?
+// https://golang.org/pkg/regexp/#example_Regexp_SubexpNames
+
+func extractSchema(uri string) (SchemaParts, error) {
+	schema_pattern := regexp.MustCompile(SCHEMA_URI_REGEX)
+
+	match := schema_pattern.FindStringSubmatch(uri)
+	if match != nil {
+		// fmt.Println(match)
+		return SchemaParts{
+			Protocol: match[1],
+			Vendor:   match[2],
+			Name:     match[3],
+			Format:   match[4],
+			Model:    match[5],
+			Revision: match[6],
+		}, nil
+	} else {
+		return SchemaParts{}, errors.New(fmt.Sprintf("Schema '%s' does not conform to regular expression '%s'", uri, SCHEMA_URI_REGEX))
+	}
+
+}
+
+// Based on https://gist.github.com/stoewer/fbe273b711e6a06315d19552dd4d33e6#gistcomment-3673823
+// Should this avoid double underscores for 'mix_Of_Camel_And_Snake_Case'?
+func insertUnderscores(s string) string {
+	var res = make([]rune, 0, len(s))
+	for i, r := range s {
+		if unicode.IsUpper(r) && i > 0 {
+			res = append(res, '_', r)
+		} else {
+			res = append(res, r)
+		}
+		// j := i  .... add j != _ to condition above?
+	}
+	return string(res)
+}
+
+func fixSchema(prefix string, schemaUri string) (string, error) {
+	parts, err := extractSchema(schemaUri)
+	if err != nil {
+		return "", errors.Wrap(err, "Error parsing schema path")
+	}
+	vendor := strings.Replace(parts.Vendor, ".", "_", -1)
+	name := insertUnderscores(parts.Name)
+
+	return strings.ToLower(strings.Join([]string{prefix, vendor, name, parts.Model}, "_")), nil
+}
+
+func shredContexts(contexts string) ([]KeyVals, error) {
+	// fmt.Println("CONTEXTS:::::::::::::::::::::::::::::")
+	// fmt.Println(contexts)
+
+	ctxts := Contexts{}
+
+	err := json.Unmarshal([]byte(contexts), &ctxts)
+	if err != nil {
+		return nil, errors.Wrap(err, "Error unmarshaling context JSON")
+	}
+
+	var distinctContexts = make(map[string][]interface{})
+	for _, entry := range ctxts.Data {
+		key, err := fixSchema("contexts", entry.Schema) // is key a crap var name here?
+		if err != nil {
+			return nil, errors.Wrap(err, "Error parsing contexts") // Too much nesting of error wrapping?
+		}
+
+		data := entry.Data
+
+		if _, present := distinctContexts[key]; present {
+			distinctContexts[key] = append(distinctContexts[key], data)
+		} else {
+			distinctContexts[key] = make([]interface{}, 1)
+			distinctContexts[key][0] = data
+		}
+	}
+
+	out := make([]KeyVals, 0, len(distinctContexts))
+	outPointer := &out
+	for key, val := range distinctContexts {
+		*outPointer = append(*outPointer, KeyVals{key, val})
+	}
+	return out, nil
+}
+
+func shredUnstruct(unstruct string) []KeyVals {
+
+	event := UnstructEvent{}
+
+	err := json.Unmarshal([]byte(unstruct), &event)
+	if err != nil {
+		fmt.Println("error:", err)
+	}
+
+	key, err := fixSchema("unstruct", event.Data.Schema) // TODO: HAndle error
+
+	return []KeyVals{KeyVals{key, event.Data.Data}}
+}
---
+++ sdk/transform.go
@@ -0,0 +1,277 @@
+package sdk
+
+import (
+	"encoding/json"
+	"fmt"
+	"strconv"
+	"time"
+  "encoding/csv"
+  "strings"
+	// "github.com/pkg/errors"
+
+
+	//	"unicode" // For camel to snake case - consider alternative?
+  //	"errors" // TODO: Decide what to do for error handling
+	// 	"github.com/hashicorp/go-multierror"
+	//	"github.com/pkg/errors"
+)
+
+// Could use a slice/array instead maybe? Pehaps give it a test - maybe no point
+// Should be named KeyVal not KeyVals
+type KeyVals struct {
+	Key   string
+	Value interface{}
+}
+
+type ValueParser func(string, string) []KeyVals
+
+type KeyFunctionPair struct {
+	Key  string
+	Func ValueParser
+}
+
+// TODO: CHECK IF WE EVEN NEED TO DO THESE TSTAMP FUNCTIONS - OTHER SDKS JUST HANDLE IT AS A STRING
+// Is parse the correct nomenclature?
+// rename to parseTstamp?
+func parseNullableTime(timeString string) *time.Time {
+	timeLayout := "2006-01-02 15:04:05.999"
+	res, _ := time.Parse(timeLayout, timeString)
+	if time.Time.IsZero(res) {
+		return nil
+	} else {
+		return &res
+	}
+}
+
+// These should all probably return KeyVals, err
+// Also perhaps they should all return slices - because the custom contexts one returns arbitrary length...
+func parseTime(key string, value string) []KeyVals {
+	return []KeyVals{KeyVals{key, parseNullableTime(value)}}
+}
+
+func parseString(key string, value string) []KeyVals {
+	return []KeyVals{KeyVals{key, value}}
+}
+
+func parseInt(key string, value string) []KeyVals {
+	intvalue, err := strconv.Atoi(value)
+	if err != nil {
+		fmt.Println(err)
+	}
+	return []KeyVals{KeyVals{key, intvalue}}
+}
+
+func parseBool(key string, value string) []KeyVals {
+	boolvalue, err := strconv.ParseBool(value)
+	if err != nil {
+		fmt.Println(err)
+	}
+	return []KeyVals{KeyVals{key, boolvalue}}
+}
+
+func parseDouble(key string, value string) []KeyVals {
+	doubleValue, err := strconv.ParseFloat(value, 64)
+	if err != nil {
+		fmt.Println(err)
+	}
+	return []KeyVals{KeyVals{key, doubleValue}}
+}
+
+func parseContexts(key string, value string) []KeyVals {
+	out, _ := shredContexts(value)
+	return out // TODO: FIX THIS BY CHANGING ALL PARSERS TO RETURN ERRORS ALSO 
+}
+
+func parseUnstruct(key string, value string) []KeyVals {
+	return shredUnstruct(value)
+}
+
+var enrichedEventFieldTypes = [131]KeyFunctionPair{KeyFunctionPair{"app_id", parseString},
+	KeyFunctionPair{"platform", parseString},
+	KeyFunctionPair{"etl_tstamp", parseTime},
+	KeyFunctionPair{"collector_tstamp", parseTime},
+	KeyFunctionPair{"dvce_created_tstamp", parseTime},
+	KeyFunctionPair{"event", parseString},
+	KeyFunctionPair{"event_id", parseString},
+	KeyFunctionPair{"txn_id", parseInt},
+	KeyFunctionPair{"name_tracker", parseString},
+	KeyFunctionPair{"v_tracker", parseString},
+	KeyFunctionPair{"v_collector", parseString},
+	KeyFunctionPair{"v_etl", parseString},
+	KeyFunctionPair{"user_id", parseString},
+	KeyFunctionPair{"user_ipaddress", parseString},
+	KeyFunctionPair{"user_fingerprint", parseString},
+	KeyFunctionPair{"domain_userid", parseString},
+	KeyFunctionPair{"domain_sessionidx", parseInt},
+	KeyFunctionPair{"network_userid", parseString},
+	KeyFunctionPair{"geo_country", parseString},
+	KeyFunctionPair{"geo_region", parseString},
+	KeyFunctionPair{"geo_city", parseString},
+	KeyFunctionPair{"geo_zipcode", parseString},
+	KeyFunctionPair{"geo_latitude", parseDouble},
+	KeyFunctionPair{"geo_longitude", parseDouble},
+	KeyFunctionPair{"geo_region_name", parseString},
+	KeyFunctionPair{"ip_isp", parseString},
+	KeyFunctionPair{"ip_organization", parseString},
+	KeyFunctionPair{"ip_domain", parseString},
+	KeyFunctionPair{"ip_netspeed", parseString},
+	KeyFunctionPair{"page_url", parseString},
+	KeyFunctionPair{"page_title", parseString},
+	KeyFunctionPair{"page_referrer", parseString},
+	KeyFunctionPair{"page_urlscheme", parseString},
+	KeyFunctionPair{"page_urlhost", parseString},
+	KeyFunctionPair{"page_urlport", parseInt},
+	KeyFunctionPair{"page_urlpath", parseString},
+	KeyFunctionPair{"page_urlquery", parseString},
+	KeyFunctionPair{"page_urlfragment", parseString},
+	KeyFunctionPair{"refr_urlscheme", parseString},
+	KeyFunctionPair{"refr_urlhost", parseString},
+	KeyFunctionPair{"refr_urlport", parseInt},
+	KeyFunctionPair{"refr_urlpath", parseString},
+	KeyFunctionPair{"refr_urlquery", parseString},
+	KeyFunctionPair{"refr_urlfragment", parseString},
+	KeyFunctionPair{"refr_medium", parseString},
+	KeyFunctionPair{"refr_source", parseString},
+	KeyFunctionPair{"refr_term", parseString},
+	KeyFunctionPair{"mkt_medium", parseString},
+	KeyFunctionPair{"mkt_source", parseString},
+	KeyFunctionPair{"mkt_term", parseString},
+	KeyFunctionPair{"mkt_content", parseString},
+	KeyFunctionPair{"mkt_campaign", parseString},
+	KeyFunctionPair{"contexts", parseContexts},
+	KeyFunctionPair{"se_category", parseString},
+	KeyFunctionPair{"se_action", parseString},
+	KeyFunctionPair{"se_label", parseString},
+	KeyFunctionPair{"se_property", parseString},
+	KeyFunctionPair{"se_value", parseString},
+	KeyFunctionPair{"unstruct_event", parseUnstruct},
+	KeyFunctionPair{"tr_orderid", parseString},
+	KeyFunctionPair{"tr_affiliation", parseString},
+	KeyFunctionPair{"tr_total", parseDouble},
+	KeyFunctionPair{"tr_tax", parseDouble},
+	KeyFunctionPair{"tr_shipping", parseDouble},
+	KeyFunctionPair{"tr_city", parseString},
+	KeyFunctionPair{"tr_state", parseString},
+	KeyFunctionPair{"tr_country", parseString},
+	KeyFunctionPair{"ti_orderid", parseString},
+	KeyFunctionPair{"ti_sku", parseString},
+	KeyFunctionPair{"ti_name", parseString},
+	KeyFunctionPair{"ti_category", parseString},
+	KeyFunctionPair{"ti_price", parseDouble},
+	KeyFunctionPair{"ti_quantity", parseInt},
+	KeyFunctionPair{"pp_xoffset_min", parseInt},
+	KeyFunctionPair{"pp_xoffset_max", parseInt},
+	KeyFunctionPair{"pp_yoffset_min", parseInt},
+	KeyFunctionPair{"pp_yoffset_max", parseInt},
+	KeyFunctionPair{"useragent", parseString},
+	KeyFunctionPair{"br_name", parseString},
+	KeyFunctionPair{"br_family", parseString},
+	KeyFunctionPair{"br_version", parseString},
+	KeyFunctionPair{"br_type", parseString},
+	KeyFunctionPair{"br_renderengine", parseString},
+	KeyFunctionPair{"br_lang", parseString},
+	KeyFunctionPair{"br_features_pdf", parseBool},
+	KeyFunctionPair{"br_features_flash", parseBool},
+	KeyFunctionPair{"br_features_java", parseBool},
+	KeyFunctionPair{"br_features_director", parseBool},
+	KeyFunctionPair{"br_features_quicktime", parseBool},
+	KeyFunctionPair{"br_features_realplayer", parseBool},
+	KeyFunctionPair{"br_features_windowsmedia", parseBool},
+	KeyFunctionPair{"br_features_gears", parseBool},
+	KeyFunctionPair{"br_features_silverlight", parseBool},
+	KeyFunctionPair{"br_cookies", parseBool},
+	KeyFunctionPair{"br_colordepth", parseString},
+	KeyFunctionPair{"br_viewwidth", parseInt},
+	KeyFunctionPair{"br_viewheight", parseInt},
+	KeyFunctionPair{"os_name", parseString},
+	KeyFunctionPair{"os_family", parseString},
+	KeyFunctionPair{"os_manufacturer", parseString},
+	KeyFunctionPair{"os_timezone", parseString},
+	KeyFunctionPair{"dvce_type", parseString},
+	KeyFunctionPair{"dvce_ismobile", parseBool},
+	KeyFunctionPair{"dvce_screenwidth", parseInt},
+	KeyFunctionPair{"dvce_screenheight", parseInt},
+	KeyFunctionPair{"doc_charset", parseString},
+	KeyFunctionPair{"doc_width", parseInt},
+	KeyFunctionPair{"doc_height", parseInt},
+	KeyFunctionPair{"tr_currency", parseString},
+	KeyFunctionPair{"tr_total_base", parseDouble},
+	KeyFunctionPair{"tr_tax_base", parseDouble},
+	KeyFunctionPair{"tr_shipping_base", parseDouble},
+	KeyFunctionPair{"ti_currency", parseString},
+	KeyFunctionPair{"ti_price_base", parseDouble},
+	KeyFunctionPair{"base_currency", parseString},
+	KeyFunctionPair{"geo_timezone", parseString},
+	KeyFunctionPair{"mkt_clickid", parseString},
+	KeyFunctionPair{"mkt_network", parseString},
+	KeyFunctionPair{"etl_tags", parseString},
+	KeyFunctionPair{"dvce_sent_tstamp", parseTime},
+	KeyFunctionPair{"refr_domain_userid", parseString},
+	KeyFunctionPair{"refr_device_tstamp", parseTime},
+	KeyFunctionPair{"derived_contexts", parseContexts},
+	KeyFunctionPair{"domain_sessionid", parseString},
+	KeyFunctionPair{"derived_tstamp", parseTime},
+	KeyFunctionPair{"event_vendor", parseString},
+	KeyFunctionPair{"event_name", parseString},
+	KeyFunctionPair{"event_format", parseString},
+	KeyFunctionPair{"event_version", parseString},
+	KeyFunctionPair{"event_fingerprint", parseString},
+	KeyFunctionPair{"true_tstamp", parseTime}}
+
+var latitudeIndex = 22
+var longitudeIndex = 23
+
+// CSV string to be parsed before this function.
+// Maybe this should be goodEventToMap
+// event is slice because csv package outputs a slice.
+// TODO: figure out how to make it a fixed-length array.
+func jsonifyGoodEvent(event []string, knownFields [131]KeyFunctionPair, addGeolocationData bool) []byte {
+
+  if len(event) != len(knownFields) {
+    fmt.Println("Wrong number of fields")
+  } else {
+    output := make(map[string]interface{})
+    if addGeolocationData && event[latitudeIndex] != "" && event[longitudeIndex] != "" {
+      output["geo_location"] = event[latitudeIndex] + "," + event[longitudeIndex]
+    }
+    for index, value := range event {
+      // skip if empty
+      if event[index] != "" {
+        // apply function if not empty
+        kVPair := knownFields[index].Func(knownFields[index].Key, value)
+        // append all results
+        for _, pair := range kVPair {
+          output[pair.Key] = pair.Value
+        }
+      }
+    }
+    jsonOutput, err := json.Marshal(output)
+    if err != nil {
+      fmt.Println(err)
+    }
+    return jsonOutput
+  }
+  // TODO: Sort return value for unhappy path
+  // TODO: Figure out how to split everything up into sensible functions
+  return nil
+}
+
+// Since Golang tries its hardest to design against optional parameters, electing to implement the main Transform function
+// to mirror the most common usage of the function - with addGeolocationData set to true (ie the default in the other SDKs).
+// TODO: Design decisions to be made around what other functions to implement/expose - or how to approach doing other things
+// One option: A method to transform only a specific set of atomic fields - which can be configured to transform all fields without doing the golocation bit...
+func Transform(event string) []byte {
+
+  // I think I prefer to just strings.Split("/t") if we can get away with it. Removes an import, and removes the need to needlessly memory on this reader object too/
+
+  r := csv.NewReader(strings.NewReader(event))
+	r.Comma = '\t'
+	r.LazyQuotes = true
+
+	record, err := r.Read()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+  return jsonifyGoodEvent(record, enrichedEventFieldTypes, true)
+}
