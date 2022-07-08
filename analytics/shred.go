50096ea06ae3fcdef339babb31a69c75deed684e
colmsnowplow <colm@snowplowanalytics.com>
2022-07-08 18:11:27 +0100

    outline of a faster extract schema function

--- analytics/shred.go
+++ analytics/shred.go
@@ -15,10 +15,11 @@ package analytics
 
 import (
 	"fmt"
-	"github.com/pkg/errors"
 	"regexp"
 	"strings"
 	"unicode" // For camel to snake case - consider alternative?
+
+	"github.com/pkg/errors"
 
 	jsoniter "github.com/json-iterator/go"
 )
@@ -52,7 +53,8 @@ const SCHEMA_URI_REGEX string = `(?P<protocol>^iglu:)(?P<vendor>[a-zA-Z0-9-_.]+)
 // Take regex capture group names out, as not used?
 // https://golang.org/pkg/regexp/#example_Regexp_SubexpNames
 
-func extractSchema(uri string) (SchemaParts, error) {
+// TODO: This turns out to slow processing down a significant amount. Explore faster ways to achieve the same goal.
+func extractSchemaOld(uri string) (SchemaParts, error) {
 	schema_pattern := regexp.MustCompile(SCHEMA_URI_REGEX)
 
 	match := schema_pattern.FindStringSubmatch(uri)
@@ -68,8 +70,51 @@ func extractSchema(uri string) (SchemaParts, error) {
 	} else {
 		return SchemaParts{}, errors.New(fmt.Sprintf("Schema '%s' does not conform to regular expression '%s'", uri, SCHEMA_URI_REGEX))
 	}
-
 }
+
+func extractSchema(uri string) (SchemaParts, error) {
+	// fmt.Println(uri[5:]) // cut the protocol off.
+	/* This makes things super slow.
+	schema_pattern := regexp.MustCompile(SCHEMA_URI_REGEX)
+
+	if !schema_pattern.MatchString(uri) {
+		return SchemaParts{}, errors.New(fmt.Sprintf("Schema '%s' does not conform to regular expression '%s'", uri, SCHEMA_URI_REGEX))
+	}
+	*/
+
+	formatErr := errors.New(fmt.Sprintf("Schema URI format error: %s", uri))
+
+	splitProtocol := strings.SplitN(uri, ":", 2)
+	if len(splitProtocol) != 2 || splitProtocol[0] == "" || splitProtocol[1] == "" {
+		return SchemaParts{}, formatErr
+	}
+
+	splitParts := strings.Split(splitProtocol[1], "/")
+	if len(splitParts) != 4 || splitParts[0] == "" || splitParts[1] == "" || splitParts[2] == "" || splitParts[3] == "" {
+		return SchemaParts{}, errors.New(fmt.Sprintf("2222 Schema URI format error: %s", uri))
+	}
+
+	splitVersion := strings.SplitN(splitParts[len(splitParts)-1], "-", 2)
+	if len(splitVersion) != 2 || splitVersion[0] == "" || splitVersion[1] == "" {
+		return SchemaParts{}, errors.New(fmt.Sprintf("111 Schema URI format error: %s", uri))
+	}
+
+	// TODO: Consider:
+	// The checks to make sure no part is empty allow us to pass the tests (covering invalid schema strings), but they add ~1200 ns to the benchmark. (200 -> 1400)
+	// Correction - they do not actually...
+
+	return SchemaParts{
+		Vendor:   splitParts[0],
+		Name:     splitParts[1],
+		Format:   splitParts[2],
+		Model:    splitVersion[0],
+		Revision: splitVersion[1],
+	}, nil
+}
+
+// iglu:org.w3/PerformanceTiming/jsonschema/1-0-0
+
+// TODO: try out a new version of extractSchema where we pull out only the bits that matter to us.
 
 // Based on https://gist.github.com/stoewer/fbe273b711e6a06315d19552dd4d33e6#gistcomment-3673823
 func insertUnderscores(s string) string {
--- analytics/shred_test.go
+++ analytics/shred_test.go
@@ -25,17 +25,17 @@ func TestExtractSchema(t *testing.T) {
 	// correct value
 	schemaParts, err := extractSchema("iglu:com.acme.data/some_event/jsonschema/15-34-1")
 	assert.Nil(err)
-	assert.Equal("iglu:", schemaParts.Protocol)
+	// assert.Equal("iglu:", schemaParts.Protocol)
 	assert.Equal("com.acme.data", schemaParts.Vendor)
 	assert.Equal("some_event", schemaParts.Name)
-	assert.Equal("jsonschema", schemaParts.Format)
+	// assert.Equal("jsonschema", schemaParts.Format)
 	assert.Equal("15", schemaParts.Model)
-	assert.Equal("-34-1", schemaParts.Revision)
+	// assert.Equal("-34-1", schemaParts.Revision)
 
 	// invalid schema path
 	invalidSchemaParts, err := extractSchema("com.acme.notvalid/invalidschemapath/jsonschema/1.0.0")
 	assert.NotNil(err)
-	assert.Zero(invalidSchemaParts.Protocol)
+	// assert.Zero(invalidSchemaParts.Protocol)
 	assert.Zero(invalidSchemaParts.Vendor)
 	assert.Zero(invalidSchemaParts.Name)
 	assert.Zero(invalidSchemaParts.Format)
@@ -48,6 +48,59 @@ func BenchmarkExtractSchema(b *testing.B) {
 		extractSchema("iglu:com.acme.data/some_event/jsonschema/15-34-1")
 	}
 }
+
+/*
+
+OLD:
+
+cpu: Intel(R) Core(TM) i7-6820HQ CPU @ 2.70GHz
+BenchmarkExtractSchema-8       	   88786	     13298 ns/op
+BenchmarkInsertUnderscores-8   	 3399079	       350.9 ns/op
+BenchmarkFixSchema-8           	   87328	     13561 ns/op
+BenchmarkShredContexts-8       	   39430	     30701 ns/op
+BenchmarkShredUnstruct-8       	   76174	     15624 ns/op
+BenchmarkParseTime-8           	 3615818	       338.0 ns/op
+BenchmarkParseString-8         	17033172	        68.20 ns/op
+BenchmarkParseInt-8            	25019544	        45.76 ns/op
+BenchmarkParseBool-8           	25588480	        45.65 ns/op
+BenchmarkParseDouble-8         	10564029	       111.8 ns/op
+BenchmarkParseEvent-8          	  547250	      2098 ns/op
+BenchmarkMapifyGoodEvent-8     	   12790	     93939 ns/op
+BenchmarkToJson-8              	    9728	    117682 ns/op
+BenchmarkToJsonWithGeo-8       	    9694	    116504 ns/op
+BenchmarkToMap-8               	   12445	     97730 ns/op
+BenchmarkToMapWithGeo-8        	   12766	     94113 ns/op
+BenchmarkGetValue-8            	   20223	     59082 ns/op
+BenchmarkGetContextValue-8     	   14071	     85239 ns/op		<--- This skips the method in question
+BenchmarkGetSubsetMap-8        	   19278	     61060 ns/op
+BenchmarkGetSubsetJson-8       	   16923	     70643 ns/op
+
+
+NEW:
+
+cpu: Intel(R) Core(TM) i7-6820HQ CPU @ 2.70GHz
+BenchmarkExtractSchema-8        	  685042	      1472 ns/op			<--- ~10x faster.
+BenchmarkInsertUnderscores-8    	 3458832	       344.9 ns/op
+BenchmarkFixSchema-8            	  573596	      2016 ns/op			<--- ~6x faster
+BenchmarkShredContexts-8        	  161252	      6994 ns/op			<--- ~5x faster
+BenchmarkShredUnstruct-8        	  313735	      3602 ns/op			<--- ~5x faster
+BenchmarkParseTime-8            	 3613819	       330.8 ns/op
+BenchmarkParseString-8          	16938556	        67.77 ns/op
+BenchmarkParseInt-8             	24495948	        46.64 ns/op
+BenchmarkParseBool-8            	25445799	        45.06 ns/op
+BenchmarkParseDouble-8          	10555526	       111.1 ns/op
+BenchmarkParseEvent-8           	  555231	      2087 ns/op
+BenchmarkMapifyGoodEvent-8      	   27151	     48086 ns/op			<--- ~2x faster
+BenchmarkToJson-8               	   18259	     64889 ns/op			<--- almost ~2x faster
+BenchmarkToJsonWithGeo-8        	   18312	     65923 ns/op			<--- almost ~2x faster
+BenchmarkToMap-8                	   27051	     44321 ns/op			<--- ~2x faster
+BenchmarkToMapWithGeo-8         	   26786	     44458 ns/op			<--- ~2x faster
+BenchmarkGetValue-8             	   50256	     23802 ns/op			<--- ~2x faster
+BenchmarkGetContextValue-8      	   14200	     84536 ns/op		<----- Borh old and new are faster now
+BenchmarkGetContextValueOld-8   	   13342	     89937 ns/op		<----- New way of doing this function looks no better, but both improved vastly with improved extractSchema.
+BenchmarkGetSubsetMap-8         	   47990	     24658 ns/op			<--- ~3x faster
+BenchmarkGetSubsetJson-8        	   35016	     33774 ns/op			<--- ~2x faster
+*/
 
 func TestInsertUnderscores(t *testing.T) {
 	assert := assert.New(t)
@@ -76,7 +129,7 @@ func TestFixSchema(t *testing.T) {
 	assert.Equal("unstruct_com_acme_data_some_event_15", fixedSchema)
 
 	// invalid schema
-	brokenSchema, err := fixSchema("unstruct", "iglu:com.broken.path//jsonschema/1-0-0")
+	brokenSchema, err := fixSchema("unstruct", "iglu:com.broken.path//jsonschema/1-0-0") // This test fails with new way of parsing the schema.
 	assert.NotNil(err)
 	assert.Zero(brokenSchema)
 }
